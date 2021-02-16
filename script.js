00690bf84dcd445e60bc4be68c97f20dad0b9fb1
Priscila Patricio <priscilapatricio2@gmail.com>
2021-02-16 12:39:08 -0300

    criação do controle das informações da tela da urna

---
+++ script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.div-1-1 span');
+let cargo = document.querySelector('.div-1-2 span');
+let descricao = document.querySelector('.div-1-4');
+let aviso = document.querySelector('.divisao-2');
+let lateral = document.querySelector('.div-1-right');
+let numeros = document.querySelector('.div-1-3');
