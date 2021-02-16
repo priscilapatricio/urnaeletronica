e575bb8849ba6ccd1102df04fd93cd1a47828f51
Priscila Patricio <priscilapatricio2@gmail.com>
2021-02-15 22:35:40 -0300

    Criação do layout e definição das etapas de funcionamento da urna.

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Joana Pereira',
+                partido: 'Partido dos Esportes',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereadora'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Bob Furuya',
+                partido: 'Partido das Artes',
+                fotos:[
+                    {url:'77222.jpg.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Claudia Santos',
+                partido: 'Partido dos Esportes',
+                vice: 'Renato Garcia',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Marcela Gonçalves',
+                partido: 'Partido das Artes',
+                vice: 'Fernando Ramos',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+];
