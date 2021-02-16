5255d90f1866985b86afcdabc0cabbeec79bf05c
Priscila Patricio <priscilapatricio2@gmail.com>
2021-02-16 12:49:23 -0300

    Ações de click nos botões do teclado

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let descricao = document.querySelector('.div-1-4');
 let aviso = document.querySelector('.divisao-2');
 let lateral = document.querySelector('.div-1-right');
 let numeros = document.querySelector('.div-1-3');
+
+function clicou(n){
+    alert("Clicou em "+n);
+}
+function branco(n){
+    alert("Clicou em BRANCO!");
+}
+function corrige(n){
+    alert("Clicou em CORRIGE!");
+}
+function confirma(n){
+    alert("Clicou em CONFIRMA!");
+}
